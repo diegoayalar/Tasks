f4a602cd0c0bf29a913eeea025240908726b252f
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-06-21 16:44:23 -0700

    Add routes for PUT and DELETE

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.route("/").get(userController.getUsers).post(userController.createUser);
+router.route("/").get(userController.getUsers).post(userController.createUser)
+router.route("/:id").get(userController.getUser).put(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;
