c498de59caacce98253b98aadfd995f92f6975c7
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-06-24 13:25:31 -0700

    Formatted userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,11 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.route("/").get(userController.getUsers).post(userController.createUser)
-router.route("/:id").get(userController.getUser).put(userController.updateUser).delete(userController.deleteUser);
+router.route("/").get(userController.getUsers).post(userController.createUser);
+router
+    .route("/:id")
+    .get(userController.getUser)
+    .put(userController.updateUser)
+    .delete(userController.deleteUser);
 
 module.exports = router;
