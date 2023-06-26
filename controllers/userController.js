00d84660db2dfbf6e7b7d02da51e55f74f909c55
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-06-25 17:26:05 -0700

    Refactor endpoint to use asyncErrorHandler middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,109 +1,69 @@
 const userModel = require("../models/user");
+const CustomError = require("../utils/customError");
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
-exports.createUser = async (req, res) => {
-    try {
-        const newUser = await userModel.create(req.body);
-        res.status(201).json({
-            status: "sucess",
-            data: {
-                user: newUser,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+exports.createUser = asyncErrorHandler(async (req, res, next) => {
+    const newUser = await userModel.create(req.body);
+    res.status(201).json({
+        status: "sucess",
+        data: {
+            user: newUser,
+        },
+    });
+});
 
-exports.updateUser = async (req, res) => {
-    try {
-        const user = await userModel.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!user) {
-            return res.status(404).json({
-                status: "fail",
-                message: "User not found",
-            });
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                user,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
+exports.updateUser = asyncErrorHandler(async (req, res, next) => {
+    const user = await userModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    if (!user) {
+        const error = new CustomError("User not found", 404);
+        return next(error);
     }
-};
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+});
 
-exports.deleteUser = async (req, res) => {
-    try {
-        const user = await userModel.findByIdAndDelete(req.params.id);
-        if (!user) {
-            return res.status(404).json({
-                status: "fail",
-                message: "User not found",
-            });
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                user,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
+exports.deleteUser = asyncErrorHandler(async (req, res, next) => {
+    const user = await userModel.findByIdAndDelete(req.params.id);
+    if (!user) {
+        const error = new CustomError("User not found", 404);
+        return next(error);
     }
-};
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+});
 
-exports.getUser = async (req, res) => {
-    try {
-        const user = await userModel.findById(req.params.id);
-        if (!user) {
-            return res.status(404).json({
-                status: "fail",
-                message: "User not found",
-            });
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                user,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
+exports.getUser = asyncErrorHandler(async (req, res, next) => {
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+        const error = new CustomError("User not found", 404);
+        return next(error);
     }
-};
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+});
 
-exports.getUsers = async (req, res) => {
-    try {
-        const users = await userModel.find();
-        console.log(users);
-        res.status(200).json({
-            status: "success",
-            data: {
-                users,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            message: err,
-        });
-    }
-};
+exports.getUsers = asyncErrorHandler(async (req, res, next) => {
+    const users = await userModel.find();
+    console.log(users);
+    res.status(200).json({
+        status: "success",
+        data: {
+            users,
+        },
+    });
+});
