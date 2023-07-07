2cb87a4d22f85d879cca26b538722169c7c99b95
MiguelCiulog <36050843+MiguelCiulog@users.noreply.github.com>
2023-07-07 00:19:33 -0700

    Initial frontend

---
+++ Front/index.js
@@ -0,0 +1,6 @@
+var redBoxes = document.getElementsByClassName('status_box');
+redBoxes[0].style.backgroundColor = 'red';
+redBoxes[1].style.backgroundColor = 'red';
+redBoxes[2].style.backgroundColor = 'yellow';
+redBoxes[3].style.backgroundColor = 'blue';
+redBoxes[4].style.backgroundColor = 'blue';
